58236061cfe79dd2105a29a8221e7492445a50c3
Evgeniy <75331348+y0nkers@users.noreply.github.com>
2022-03-27 14:02:24 +0300

    Add files via upload

---
+++ FormMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace FileEditor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFile.Location = new System.Drawing.Point(12, 27);
+            this.textBoxFile.Multiline = true;
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxFile.Size = new System.Drawing.Size(776, 411);
+            this.textBoxFile.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveFileToolStripMenuItem,
+            this.closeFileToolStripMenuItem,
+            this.exitFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "Открыть";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveFileToolStripMenuItem.Text = "Сохранить";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // closeFileToolStripMenuItem
+            // 
+            this.closeFileToolStripMenuItem.Name = "closeFileToolStripMenuItem";
+            this.closeFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeFileToolStripMenuItem.Text = "Закрыть";
+            this.closeFileToolStripMenuItem.Click += new System.EventHandler(this.closeFileToolStripMenuItem_Click);
+            // 
+            // exitFileToolStripMenuItem
+            // 
+            this.exitFileToolStripMenuItem.Name = "exitFileToolStripMenuItem";
+            this.exitFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitFileToolStripMenuItem.Text = "Выход";
+            this.exitFileToolStripMenuItem.Click += new System.EventHandler(this.exitFileToolStripMenuItem_Click);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFileName.Location = new System.Drawing.Point(248, 1);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.ReadOnly = true;
+            this.textBoxFileName.Size = new System.Drawing.Size(118, 23);
+            this.textBoxFileName.TabIndex = 2;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(179, 7);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(67, 13);
+            this.labelFileName.TabIndex = 3;
+            this.labelFileName.Text = "Имя файла:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "FormMain";
+            this.Text = "Редактор файла";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitFileToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Label labelFileName;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileEditor
+{
+    public partial class FormMain : Form
+    {
+        private string filePath;
+        private FileSystemWatcher watcher;
+        private bool isSaving = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            textBoxFile.ReadOnly = true;
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "";
+                openFileDialog.Filter = "Все файлы|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    var fileStream = openFileDialog.OpenFile();
+                    using (var reader = new StreamReader(fileStream))
+                    {
+                        textBoxFile.Text = reader.ReadToEnd(); // Считываем всё содержимое файла
+                    }
+                }
+            }
+
+            if (watcher != null) watcher.Dispose();
+            if (filePath != null)
+            {
+                textBoxFile.ReadOnly = false;
+                var fileName = Path.GetFileName(filePath); // Получаем имя файла
+                var directoryPath = filePath.Substring(0, filePath.Length - fileName.Length); // Получаем путь до файла
+                textBoxFileName.Text = fileName;
+                watcher = new FileSystemWatcher(directoryPath, fileName);
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                watcher.SynchronizingObject = this;
+                watcher.Changed += (obj, evnt) => {
+                    if (isSaving)
+                    {
+                        isSaving = false;
+                        return;
+                    }
+                    var reload = MessageBox.Show(filePath+"\n\nЭтот файл был изменён другой программой.\nВы хотите обновить его?", "Обновление файла", MessageBoxButtons.YesNo);
+                    if (reload == DialogResult.Yes)
+                    {
+                        using (var reader = new StreamReader(filePath))
+                        {
+                            textBoxFile.Text = reader.ReadToEnd();
+                        }
+                    }
+                };
+            }
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filePath == null)
+            {
+                MessageBox.Show("Нет открытого файла!", "Сохранение файла");
+                return;
+            }
+
+            isSaving = true;
+            using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                var writer = new StreamWriter(fileStream);
+                writer.Write(textBoxFile.Text);
+                writer.Close();
+            }
+        }
+
+        private void closeFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filePath == null)
+            {
+                MessageBox.Show("Нет открытого файла!", "Закрытие файла");
+                return;
+            }
+            textBoxFile.ReadOnly = true;
+            textBoxFile.Text = "";
+            textBoxFileName.Text = "";
+            filePath = null;
+        }
+
+        private void exitFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
